ec9dd5fab16c9c425836f8fb3d020fbc6ef4b2eb
IvoCarrascoMediaAgency <40969344+IvoCarrascoMediaAgency@users.noreply.github.com>
2019-08-25 08:34:06 -0500

    2

---
+++ anim.js
@@ -0,0 +1,7 @@
+var animation_fondo_cohete_home = bodymovin.loadAnimation({
+    container: document.getElementById('lupo1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'json/lupo.json',  
+    })
